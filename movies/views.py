c1531cfc7f955d5b17917ea400c43d3529d64638
Rohanop <rohankamble3011@gmail.com>
2022-04-23 19:46:03 +0530

    Add: search filter

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
-from rest_framework.filters import OrderingFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 from .models import Movie
 from .serializers import MovieSerializer
@@ -9,6 +9,8 @@ from .serializers import MovieSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'date_of_release']
 
 
 class MovieListAPIView(ListAPIView):
